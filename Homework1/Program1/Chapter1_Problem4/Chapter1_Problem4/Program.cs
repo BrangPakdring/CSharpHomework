79fe811e7f9549ac8863ad0a575cdd17caf49a90
BrangPakdring <Brang.Pakdring@gmail.com>
2018-09-09 20:08:57 +0800

    finished c1_p4

---
+++ Homework1/Program1/Chapter1_Problem4/Chapter1_Problem4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chapter1_Problem4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var a = Convert.ToDouble(Console.ReadLine());
+			var b = double.Parse(Console.ReadLine());
+			Console.WriteLine(a * b);
+		}
+	}
+}
