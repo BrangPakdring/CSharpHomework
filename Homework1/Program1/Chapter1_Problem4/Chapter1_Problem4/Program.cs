5ff4c445825a7bf2f709ba90c8a19a62e5ae79c1
BrangPakdring <Brang.Pakdring@gmail.com>
2018-09-09 20:55:18 +0800

    finished c1_p5

--- Homework1/Program1/Chapter1_Problem4/Chapter1_Problem4/Program.cs
+++ Homework1/Program1/Chapter1_Problem4/Chapter1_Problem4/Program.cs
@@ -6,9 +6,11 @@ namespace Chapter1_Problem4
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("input number 1: ");
 			var a = Convert.ToDouble(Console.ReadLine());
+			Console.Write("input number 2: ");
 			var b = double.Parse(Console.ReadLine());
-			Console.WriteLine(a * b);
+			Console.WriteLine("result: " + a * b);
 		}
 	}
 }
---
+++ Homework1/Program2/Chapter1_Problem5/Chapter1_Problem5/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter1_Problem5
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var number1 = Number1.Text;
+			var number2 = Number2.Text;
+			if (number1 == "" || number2 == "")
+			{
+				Result.Text = "invalid";
+			}
+			else
+			{
+				var res = Convert.ToString(double.Parse(number1) * double.Parse(number2));
+				Result.Text = res;
+			}
+		}
+
+		private void Number1_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private void Number2_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+	}
+}
