6913f9a2176d1edab70869f95ed2d4839a26d8ae
Brang Pakdring <Brang.Pakdring@gmail.com>
2018-10-21 10:19:45 +0800

    unit test demo

---
+++ Homework6/Program1Tests/OrderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
