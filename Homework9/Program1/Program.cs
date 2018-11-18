564f575eab74c1bf5614d1584f11f18dff6bcee1
Brang Pakdring <Brang.Pakdring@gmail.com>
2018-11-18 22:22:21 +0800

    details modified

--- Homework9/Program1/Program.cs
+++ Homework9/Program1/Program.cs
@@ -7,16 +7,33 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Program1
 {
 	internal class Program
 	{
-		private const ushort PageLimit = 100;
-		private const uint TimeLimit = 20;
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Test using single thread:");
+			new Program().Run(false);
+			Console.WriteLine();
+			Console.WriteLine("Test using optimized thread:");
+			new Program().Run(true);
+		}
+
+		private const uint ThreadLimit = 128;
+		
+		private const ushort PageLimit = 500;
+
+		private const uint TimeLimit = 50;
+
 		private Hashtable _urls;
+
 		private int _count;
+
 		private string startURL = "https://blog.csdn.net/lttree/article/category/2397059";
+
 		private List<Thread> _threads;
 
 		public Program()
@@ -27,27 +44,24 @@ namespace Program1
 			_threads = new List<Thread>();
 		}
 
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Test using single thread:");
-			new Program().Run(false);
-			Console.WriteLine();
-			Console.WriteLine("Test using optimized thread:");
-			new Program().Run(true);
-			
-		}
-
-		void Run(bool parallel = false)
+		void Run(bool parallel)
 		{
 			Program program = new Program();
-			
+
 			Stopwatch stopwatch = new Stopwatch();
-			Thread thread = new Thread(() => program.Crawl(parallel));
-			_threads.Add(thread);
+
+			var lim = parallel == false ? 1
+				: Math.Min(PageLimit, ThreadLimit);
 
 			Console.WriteLine("Start crawling.");
 			stopwatch.Start();
-			thread.Start();
+			for (var i = 0; i < lim; ++i)
+			{
+				Thread thread = new Thread(() => program.Crawl(parallel)) {Name = i.ToString()};
+				_threads.Add(thread);
+				thread.Start();
+			}
+
 //			thread.Join();
 			int cnt = 0;
 			while (program._count < PageLimit)
@@ -57,71 +71,89 @@ namespace Program1
 				if (cnt > TimeLimit)
 				{
 					Console.WriteLine("Time limit exceeded.");
-					foreach (var thread1 in _threads)
+					lock (this)
 					{
-						thread1.Abort();
+						foreach (var thread1 in _threads)
+						{
+							thread1.Abort();
+						}
 					}
 
 					break;
 				}
+
 				Thread.Sleep(1000);
 			}
-			
+
 			stopwatch.Stop();
 			Console.WriteLine("Time elapsed: " + stopwatch.ElapsedMilliseconds + ".");
 		}
 
-		private void Crawl(bool parallel = false)
+		private void Crawl(bool parallel)
 		{
 			string html = "";
-			do
+			while (true)
 			{
 				string current = null;
+				int count;
+				foreach (DictionaryEntry dictionaryEntry in _urls)
+				{
+					if ((bool) dictionaryEntry.Value) continue;
+					current = dictionaryEntry.Key as string;
+					if (current == null) continue;
+					break;
+				}
+
 				lock (this)
 				{
-					foreach (DictionaryEntry dictionaryEntry in _urls)
-					{
-						if ((bool) dictionaryEntry.Value) continue;
-						current = dictionaryEntry.Key as string;
-						_urls[current] = true;
-						++_count;
-						break;
-					}
+					if (current == null) return;
+					_urls[current] = true;
+					++_count;
+					if (_count >= PageLimit) return;
+					count = _count;
 				}
 
-				if (current == null || _count >= PageLimit) return;
 				Console.WriteLine("Crawling #" + _count + ": " + current);
-				html = Download(current, _count);
+				html = Download(current, count);
+				Parse(html, parallel);
 
-				/*if (!parallel)*/
-				Parse(html);
-
-				if (parallel)
-				{
-					/*Parse(html);*/
-//					ThreadPool.QueueUserWorkItem(new WaitCallback(this.Crawl), true))
-					var thread = new Thread(() => this.Crawl(true));
-					_threads.Add(thread);
-					thread.Start();
-//					thread.Join();
-				}
-			} while (true);
+//				if (parallel)
+//				{
+//					lock (this)
+//					{
+//						var thread = new Thread(() => this.Crawl(true));
+//						thread.Name = _count.ToString();
+//						_threads.Add(thread);
+//						thread.Start();
+//					}
+//				}
+			}
 		}
 
-		private void Parse(string html)
+		private void Parse(string html, bool parallel = false)
 		{
 			string strRef = @"(href|HREF)[]*=[]*[""']http[s]?://[^""']+[""']>";
 			MatchCollection collection = new Regex(strRef).Matches(html);
-			foreach (Match match in collection)
-			{
-				strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '#', ' ', '>');
-				if (strRef.Length == 0) continue;
-				if (strRef.StartsWith("https://") == false) continue;
-				lock (this)
+
+			if (parallel)
+				Parallel.For(0, collection.Count, i =>
 				{
-					if (_urls[strRef] == null) _urls[strRef] = false;
+					var match = collection[i];
+					var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '#', ' ', '>');
+					if (url.Length <= 0 || !url.StartsWith("http")) return;
+					if (_urls[url] == null) _urls[url] = false;
+				});
+			else
+				foreach (Match match in collection)
+				{
+					strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '#', ' ', '>');
+					if (strRef.Length == 0) continue;
+					if (strRef.StartsWith("https://") == false) continue;
+					lock (this)
+					{
+						if (_urls[strRef] == null) _urls[strRef] = false;
+					}
 				}
-			}
 		}
 
 		private string Download(string url, int index)
